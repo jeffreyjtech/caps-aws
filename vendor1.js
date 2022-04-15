068cfdbfef68512c599fd2fcc122135c12db688e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-15 08:44:08 -0700

    Created vendor file which publishes to pickup sns

---
+++ vendor1.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const crypto = require('crypto');
+const Chance = require('chance');
+const chance = new Chance();
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2'});
+
+const stringifiedOrder = JSON.stringify({
+  orderId: crypto.randomUUID(),
+  customer: chance.name(),
+  queueArn: 'arn:aws:sqs:us-west-2:688409722565:vendor1-delivered',
+  queueUrl: 'https://sqs.us-west-2.amazonaws.com/688409722565/vendor1-delivered',
+});
+
+const sns = new AWS.SNS();
+
+const topic = 'arn:aws:sns:us-west-2:688409722565:pickup.fifo';
+
+const payload = {
+  Message: stringifiedOrder,
+  TopicArn: topic,
+  MessageDeduplicationId: crypto.createHash('sha256').update(stringifiedOrder, 'utf8').digest('hex'),
+  MessageGroupId: 'vendors',
+};
+
+let response = sns.publish(payload).promise()
+  .then(console.log)
+  .catch(console.error);
+
+console.log(response);
