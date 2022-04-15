28dc150c1976f9c24f9a710cdd1957ef7fb7e548
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-15 09:39:39 -0700

    Vendor now receives and logs delivery receipts

--- vendor1.js
+++ vendor1.js
@@ -5,6 +5,7 @@ const Chance = require('chance');
 const chance = new Chance();
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-west-2'});
+const { Consumer } = require('sqs-consumer');
 
 const stringifiedOrder = JSON.stringify({
   orderId: crypto.randomUUID(),
@@ -22,8 +23,27 @@ const payload = {
   MessageGroupId: 'vendor1',
 };
 
-let response = sns.publish(payload).promise()
-  .then(console.log)
+sns.publish(payload).promise()
+  .then((response) => {
+    console.log('Payload sent to SNS:\n ', response);
+  })
   .catch(console.error);
 
-console.log(response);
+
+const app = Consumer.create({
+  queueUrl: 'https://sqs.us-west-2.amazonaws.com/688409722565/vendor1-delivered',
+  handleMessage: handleDelivered,
+});
+
+function handleDelivered(message) {
+  try {
+    const order = JSON.parse(message.Body);
+    console.log(`Thank you ${order.customer} for order ${order.orderId}`);   
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.start();
+
+console.log('App has started');
